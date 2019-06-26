e78558ee9d665038f3d8b81fd509fc5fd36b5308
Bruno Varela <bruno.varela@aptoide.com>
2019-06-26 12:44:57 +0100

    confs added
    confs added

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,6 @@ cd ..
 # Add '.dev' on the version name. Non dev builds are done though jenkins build only
 export BUILD_NAME_SUFFIX=".dev"
 
-./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean
-./gradlew :appcoins:assembleDebug :android-appcoins-billing:assembleDebug :appcoins-billing:assemble
+./gradlew :appcoins:clean :android-appcoins-billing:clean :appcoins-billing:clean appcoins-ads:clean
+./gradlew :appcoins:assembleDebug :android-appcoins-billing:assembleDebug :appcoins-billing:assembleDebug appcoins-ads:assembleDebug
 ./gradlew publish bintrayUpload
